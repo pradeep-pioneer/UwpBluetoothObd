a0f35752f5afb7d48518806a882f5415642eda28
Pradeep Singh <pradeep.ansh.sumit@gmail.com>
2017-04-15 12:12:39 +0530

    Add project files.

---
+++ UwpBluetoothObd/MainPage.xaml.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Devices.SerialCommunication;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.Rfcomm;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.UI.Core;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UwpBluetoothObd
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private DeviceInformationCollection deviceCollection;
+        private DeviceInformation selectedDevice;
+        private RfcommDeviceService deviceService;
+        CoreDispatcher dispatcher = Window.Current.Dispatcher;
+        public string deviceName = "HUAWEI KII-L22";
+
+        StreamSocket streamSocket = new StreamSocket();
+        private async void InitializeRfcommServer()
+        {
+            try
+            {
+                listBoxDevices.Text = string.Empty;
+                string device1 = RfcommDeviceService.GetDeviceSelector(RfcommServiceId.SerialPort);
+                deviceCollection = await Windows.Devices.Enumeration.DeviceInformation.FindAllAsync(device1);
+                selectedDevice = deviceCollection[0];
+                await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => {
+                    foreach (var item in deviceCollection)
+                    {
+                        listBoxDevices.Text += "\n========================";
+                        listBoxDevices.Text += "\nDevice";
+                        listBoxDevices.Text += string.Format("\nName: {0}\nId: {1}", item.Name, item.Id);
+                        listBoxDevices.Text += "\n========================";
+                        listBoxDevices.Text += "\nProperties";
+                        listBoxDevices.Text += "\n========================";
+                        foreach (var prop in item.Properties)
+                        {
+                            listBoxDevices.Text += string.Format("\nName: {0} - \tValue: {1}", prop.Key, prop.Value);
+                        }
+                        listBoxDevices.Text += "\n========================";
+                    }
+                });
+                deviceService = await RfcommDeviceService.FromIdAsync(selectedDevice.Id);
+                if (deviceService != null)
+                {
+                    //connect the socket   
+                    try
+                    {
+                        await streamSocket.ConnectAsync(deviceService.ConnectionHostName, deviceService.ConnectionServiceName);
+                    }
+                    catch (Exception ex)
+                    {
+                        errorStatus.Visibility = Visibility.Visible;
+                        errorStatus.Text = "Cannot connect bluetooth device:" + ex.Message;
+                    }
+
+                }
+                else
+                {
+                    errorStatus.Visibility = Visibility.Visible;
+                    errorStatus.Text = "Didn't find the specified bluetooth device";
+                }
+            }
+            catch (Exception exception)
+            {
+                errorStatus.Visibility = Visibility.Visible;
+                errorStatus.Text = exception.Message;
+            }
+        }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            
+        }
+
+        private void buttonDiscover_Click(object sender, RoutedEventArgs e)
+        {
+            InitializeRfcommServer();
+        }
+
+        private async void buttonSend_Click(object sender, RoutedEventArgs e)
+        {
+            if (deviceService != null)
+            {
+                //send data
+                string sendData = "ATZ\r";
+                if (string.IsNullOrEmpty(sendData))
+                {
+                    errorStatus.Visibility = Visibility.Visible;
+                    errorStatus.Text = "Please specify the string you are going to send";
+                }
+                else
+                {
+                    DataWriter dwriter = new DataWriter(streamSocket.OutputStream);
+                    dwriter.WriteString(sendData);
+                    await dwriter.StoreAsync();
+                    await dwriter.FlushAsync();
+                    //await Task.Delay(TimeSpan.FromSeconds(5));
+                    AddLog(string.Format("Request: {0}", sendData));
+                    //await Task.Delay(TimeSpan.FromSeconds(5));
+                    
+                    sendData = "ATSP6\r";
+                    dwriter.WriteString(sendData);
+                    await dwriter.StoreAsync();
+                    await dwriter.FlushAsync();
+                    //await Task.Delay(TimeSpan.FromSeconds(5));
+                    AddLog(string.Format("Request: {0}", sendData));
+                    //await Task.Delay(TimeSpan.FromSeconds(5));
+                    sendData = "ATH0\r";
+                    dwriter.WriteString(sendData);
+                    await dwriter.StoreAsync();
+                    await dwriter.FlushAsync();
+                    //await Task.Delay(TimeSpan.FromSeconds(5));
+                    AddLog(string.Format("Request: {0}", sendData));
+                    //await Task.Delay(TimeSpan.FromSeconds(5));
+                    sendData = "ATCAF1\r";
+                    dwriter.WriteString(sendData);
+                    await dwriter.StoreAsync();
+                    await dwriter.FlushAsync();
+                    //await Task.Delay(TimeSpan.FromSeconds(5));
+                    AddLog(string.Format("Request: {0}", sendData));
+                    //await Task.Delay(TimeSpan.FromSeconds(5));
+                    sendData = "01 0D\r";
+                    dwriter.WriteString(sendData);
+                    await dwriter.StoreAsync();
+                    await dwriter.FlushAsync();
+                    //await Task.Delay(TimeSpan.FromSeconds(5));
+                    AddLog(string.Format("Request: {0}", sendData));
+                    //await Task.Delay(TimeSpan.FromSeconds(5));
+                    ReadData();
+                }
+
+            }
+            else
+            {
+                errorStatus.Visibility = Visibility.Visible;
+                errorStatus.Text = "Bluetooth is not connected correctly!";
+            }
+        }
+
+        private async void ReadData()
+        {
+            DataReader dreader = new DataReader(streamSocket.InputStream);
+            dreader.InputStreamOptions = InputStreamOptions.Partial;
+            uint count = await dreader.LoadAsync(512);
+            string text = dreader.ReadString(count);
+            AddLog(text);
+        }
+
+        private async void ReceiveData()
+        {
+            // read the data
+
+            DataReader dreader = new DataReader(streamSocket.InputStream);
+            uint sizeFieldCount = await dreader.LoadAsync(sizeof(uint));
+            if (sizeFieldCount != sizeof(uint))
+            {
+                return;
+            }
+
+            uint stringLength;
+            uint actualStringLength;
+
+            try
+            {
+                stringLength = dreader.ReadUInt32();
+                actualStringLength = await dreader.LoadAsync(stringLength);
+
+                if (stringLength != actualStringLength)
+                {
+                    return;
+                }
+                string text = dreader.ReadString(actualStringLength);
+                AddLog(string.Format("Response: {0}", text));
+            }
+            catch (Exception ex)
+            {
+                errorStatus.Visibility = Visibility.Visible;
+                errorStatus.Text = "Reading data from Bluetooth encountered error!" + ex.Message;
+            }
+        }
+
+        private async void AddLog(string text)
+        {
+            await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                listBoxObdCommunication.Items.Add(text);
+            });
+        }
+    }
+}
---
+++ UwpBluetoothObd/ObdData.xaml.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Bluetooth.Rfcomm;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+using Windows.Devices.Enumeration;
+using Windows.UI.Core;
+using System.Threading.Tasks;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UwpBluetoothObd
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ObdData : Page
+    {
+        CoreDispatcher dispatcher = Window.Current.Dispatcher;
+
+        private DeviceInformationCollection deviceCollection;
+        private DeviceInformation selectedDevice;
+        private RfcommDeviceService deviceService;
+
+        StreamSocket streamSocket = new StreamSocket();
+        DataReader reader;
+        DataWriter writer;
+        public ObdData()
+        {
+            this.InitializeComponent();
+        }
+
+        private async Task LogStatus(string status)
+        {
+            await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                Status.Text = string.Format("Status: {0}", status);
+            });
+        }
+
+        private async Task AddCommandLog(string value, bool isResponse = false)
+        {
+            await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                Commands.Items.Add(string.Format("{0}: {1}", isResponse?"Response":"Request", value));
+                Commands.SelectedIndex = Commands.Items.Count - 1;
+                Commands.ScrollIntoView(Commands.SelectedItem);
+            });
+        }
+        private async Task SetupConnections()
+        {
+            string device1 = RfcommDeviceService.GetDeviceSelector(RfcommServiceId.SerialPort);
+            deviceCollection = await Windows.Devices.Enumeration.DeviceInformation.FindAllAsync(device1);
+            await AddCommandLog(string.Format("Connecting to : {0}", device1));
+            await AddCommandLog(string.Format("Devices found : {0}", deviceCollection.Count));
+            selectedDevice = deviceCollection[0];
+            deviceService = await RfcommDeviceService.FromIdAsync(selectedDevice.Id);
+            if (deviceService != null)
+            {
+                await streamSocket.ConnectAsync(deviceService.ConnectionHostName, deviceService.ConnectionServiceName);
+                await LogStatus("Connection Established!");
+                await SendInitializationCommands();
+                await SetupOptions(true);
+                await LogStatus("Ready for command");
+            }
+            else
+            {
+                await SetupOptions(false);
+                throw new Exception("Didn't find the specified bluetooth device");
+            }
+        }
+        private void SetupStreams()
+        {
+            reader = new DataReader(streamSocket.InputStream);
+            reader.InputStreamOptions = InputStreamOptions.Partial;
+            writer = new DataWriter(streamSocket.OutputStream);
+        }
+        private async Task SendInitializationCommands()
+        {
+            string response = string.Empty;
+            await LogStatus("Setting Up Streams!");
+            SetupStreams();
+            await LogStatus("Streams Setup!");
+            await LogStatus("Sending Commands!");
+            await AddCommandLog("ATZ\r");
+            response = await SendCommand("ATZ\r");
+            await AddCommandLog(response, true);
+
+            await AddCommandLog("ATSP6\r");
+            response = await SendCommand("ATSP6\r");
+            await AddCommandLog(response, true);
+
+            await AddCommandLog("ATH0\r");
+            response = await SendCommand("ATH0\r");
+            await AddCommandLog(response, true);
+
+            await AddCommandLog("ATCAF1\r");
+            response = await SendCommand("ATCAF1\r");
+            await AddCommandLog(response, true);
+        }
+
+        private async Task<string> SendCommand(string command)
+        {
+            await LogStatus(string.Format("Sending Command - {0}", command));
+            writer.WriteString(command);
+            await writer.StoreAsync();
+            await writer.FlushAsync();
+            uint count = await reader.LoadAsync(512);
+            return reader.ReadString(count).Trim('>');
+        }
+
+        private async Task SetupOptions(bool enabled)
+        {
+            await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                Speed.IsEnabled = enabled;
+                Rpm.IsEnabled = enabled;
+                EngineLoad.IsEnabled = enabled;
+            });
+        }
+
+        private async Task InitializeStuff()
+        {
+            try
+            {
+                await LogStatus("Initialization Started!");
+                await SetupConnections();
+            }
+            catch(Exception ex)
+            {
+                await LogStatus(string.Format("Exception: {0}", ex.Message));
+                await SetupOptions(false);
+            }
+        }
+
+        private async void Initialize_Click(object sender, RoutedEventArgs e)
+        {
+            await InitializeStuff();
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            await SetupOptions(false);
+        }
+
+        private int decodeHexNumber(string hexString)
+        {
+            byte[] raw = new byte[hexString.Length / 2];
+            for (int i = 0; i < raw.Length; i++)
+            {
+                raw[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
+            }
+            string final = raw[0].ToString();
+            AddCommandLog(final, true);
+            return Convert.ToInt32(final);
+        }
+
+        private async void Speed_Click(object sender, RoutedEventArgs e)
+        {
+            await AddCommandLog("010D\r");
+            var response = await SendCommand("010D\r");
+            await AddCommandLog(response, true);
+            await DecodeAndShowSpeed(response);
+            await LogStatus("Ready for command");
+        }
+
+        private async Task DecodeAndShowSpeed(string response)
+        {
+            var speedHexString = response.Substring(4);
+            int speed = decodeHexNumber(speedHexString);
+            await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                RadialGaugeSpeed.Value = speed;
+            });
+        }
+
+        private async void Rpm_Click(object sender, RoutedEventArgs e)
+        {
+            await AddCommandLog("010C\r");
+            var response = await SendCommand("010C\r");
+            await AddCommandLog(response, true);
+            await DecodeAndShowRpm(response);
+            await LogStatus("Ready for command");
+        }
+
+        private async Task DecodeAndShowRpm(string response)
+        {
+            var rpmString = response.Substring(4);
+            double rpm = (Convert.ToDouble(decodeHexNumber(response.Substring(4))));
+            await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                RadialGaugeRpm.Value = rpm;
+            });
+        }
+
+        private async void EngineLoad_Click(object sender, RoutedEventArgs e)
+        {
+            await AddCommandLog("0104\r");
+            var response = await SendCommand("0104\r");
+            await AddCommandLog(response, true);
+            await DecodeAndShowEngineLoad(response);
+            await LogStatus("Ready for command");
+        }
+
+        private async Task DecodeAndShowEngineLoad(string response)
+        {
+            var rpmHexString = response.Substring(4);
+            double rpm = (100f / 255f) * (Convert.ToDouble(decodeHexNumber(rpmHexString)));
+            await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                RadialGaugeEngineLoad.Value = rpm;
+            });
+        }
+    }
+}
